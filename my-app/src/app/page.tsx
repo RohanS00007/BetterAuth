95679ed421bb3b7b8ee42681d8fb952b9055b8e9
Rohan Singh <rohansingh70112@gmail.com>
2025-12-29 00:37:08 +0530

    small css changes

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -2,7 +2,7 @@ import UserInfo from "@/components/custom/userInfo";
 
 export default function Home() {
   return (
-    <div className="flex max-h-screen min-h-[calc(100vh-80px)] w-full place-content-center place-items-center">
+    <div className="flex max-h-screen min-h-[calc(100vh-80px)] w-full place-content-center place-items-center p-6">
       <UserInfo />
     </div>
   );
